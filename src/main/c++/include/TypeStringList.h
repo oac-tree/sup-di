387d5465e0ba737e4e69a8198bb79e64aed94c72
Walter Van Herck <walter.vanherck@gmail.com>
2022-05-10 00:15:52 +0200

    Add TypeStringList class template and unit tests

---
+++ src/main/c++/include/TypeStringList.h
@@ -0,0 +1,74 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef _SUP_TypeStringList_h_
+#define _SUP_TypeStringList_h_
+
+#include <array>
+#include <string>
+#include <vector>
+
+namespace sup
+{
+namespace di
+{
+namespace internal
+{
+
+template <typename... Args>
+class TypeStringList {
+public:
+  TypeStringList(const std::vector<std::string>& string_list);
+  ~TypeStringList() = default;
+
+  template<std::size_t I> using IndexedType =
+    typename std::tuple_element<I, std::tuple<Args...>>::type;
+
+  template<std::size_t I>
+    std::string IndexedString();
+
+private:
+  std::array<std::string, sizeof...(Args)> names;
+};
+
+template <typename... Args>
+TypeStringList<Args...>::TypeStringList(const std::vector<std::string>& string_list)
+{
+  for(std::size_t i=0; i<sizeof...(Args); ++i)
+  {
+    names[i] = string_list[i];
+  }
+}
+
+template <typename... Args>
+template<std::size_t I>
+std::string TypeStringList<Args...>::IndexedString()
+{
+  return names[I];
+}
+
+}  // namespace internal
+
+}  // namespace di
+
+}  // namespace sup
+
+#endif  // _SUP_TypeStringList_h_
---
+++ src/test/c++/unit/TypeStringList-tests.cpp
@@ -0,0 +1,51 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include "TypeStringList.h"
+
+#include <type_traits>
+
+using namespace sup::di::internal;
+
+class TypeStringListTest : public ::testing::Test
+{
+protected:
+  TypeStringListTest();
+  virtual ~TypeStringListTest();
+
+  TypeStringList<int, std::string> type_string_list;
+};
+
+TEST_F(TypeStringListTest, MemberFunctions)
+{
+  EXPECT_EQ(type_string_list.IndexedString<0>(), "int");
+  EXPECT_EQ(type_string_list.IndexedString<1>(), "std::string");
+  EXPECT_TRUE((std::is_same<int, decltype(type_string_list)::IndexedType<0>>::value));
+  EXPECT_TRUE((std::is_same<std::string, decltype(type_string_list)::IndexedType<1>>::value));
+}
+
+TypeStringListTest::TypeStringListTest()
+  : type_string_list({"int", "std::string"})
+{}
+
+TypeStringListTest::~TypeStringListTest() = default;
