f63e6071b9c5155aa5bf78f2eb743894ba611584
Walter Van Herck <walter.vanherck@iter.org>
2022-05-11 09:51:38 +0000

    Add unit test for more complex object tree

--- src/test/c++/unit/ObjectManager-tests.cpp
+++ src/test/c++/unit/ObjectManager-tests.cpp
@@ -40,6 +40,7 @@ protected:
 
 const std::string HelloPrinterInstanceName = "HelloPrinterInstance";
 const std::string PrinterDecoratorInstanceName = "PrinterDecoratorInstance";
+const std::string PrinterAggregatorInstanceName = "PrinterAggregatorInstance";
 
 TEST_F(ObjectManagerTest, NoDependencies)
 {
@@ -83,6 +84,28 @@ TEST_F(ObjectManagerTest, GenericFactoryFunction)
     object_manager.GetInstance<IPrinter>(PrinterDecoratorInstanceName));
   EXPECT_NE(print_service, nullptr);
   EXPECT_EQ(print_service->Print(), DecoratedPrefix + HelloWorld);
+}
+
+TEST_F(ObjectManagerTest, ComplexObjectTree)
+{
+  EXPECT_NO_THROW(object_manager.RegisterFactoryFunction(
+    HelloPrinterName, GenericInstanceFactoryFunction<IPrinter, HelloPrinter>));
+  EXPECT_NO_THROW(object_manager.RegisterFactoryFunction(
+    PrinterDecoratorName, GenericInstanceFactoryFunction<IPrinter, PrinterDecorator, IPrinter>));
+  EXPECT_NO_THROW(object_manager.RegisterFactoryFunction(PrinterAggregatorName,
+    GenericInstanceFactoryFunction<IPrinter, PrinterAggregator, IPrinter, IPrinter>));
+
+  EXPECT_NO_THROW(object_manager.CreateInstance(HelloPrinterName, HelloPrinterInstanceName, {}));
+  EXPECT_NO_THROW(object_manager.CreateInstance(
+    PrinterDecoratorName, PrinterDecoratorInstanceName, {HelloPrinterInstanceName}));
+  EXPECT_NO_THROW(object_manager.CreateInstance(
+    PrinterAggregatorName, PrinterAggregatorInstanceName,
+    {HelloPrinterInstanceName, PrinterDecoratorInstanceName}));
+  IPrinter* print_service = nullptr;
+  EXPECT_NO_THROW(print_service =
+    object_manager.GetInstance<IPrinter>(PrinterAggregatorInstanceName));
+  EXPECT_NE(print_service, nullptr);
+  EXPECT_EQ(print_service->Print(), HelloWorld + DecoratedPrefix + HelloWorld);
 }
 
 ObjectManagerTest::ObjectManagerTest()
