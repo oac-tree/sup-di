2b7a0083521b439dd1fdfb0d283f3d73a4d8caa8
Walter Van Herck <walter.vanherck@iter.org>
2022-06-22 15:29:17 +0000

    Add main.cpp for executable

---
+++ src/main/c++/sup-di-object-manager/main.cpp
@@ -0,0 +1,25 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition of the dependency injection executable in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+int main()
+{
+  return 0;
+}
