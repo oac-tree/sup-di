6018d7e1c979cb692231c62514dfad6940027940
Walter Van Herck <walter.vanherck@iter.org>
2022-06-23 12:03:24 +0000

    Add error code header

---
+++ src/main/c++/libsup-di/sup/di/error_codes.h
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2022 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#ifndef SUP_DI_ERROR_CODES_H_
+#define SUP_DI_ERROR_CODES_H_
+
+namespace sup
+{
+namespace di
+{
+}  // namespace di
+
+}  // namespace sup
+
+#endif  // SUP_DI_ERROR_CODES_H_
