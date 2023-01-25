edeb10bc4679bfa9be72c63424d05d6f4b931e16
Walter Van Herck <walter.vanherck@iter.org>
2023-01-25 12:16:56 +0000

    Unit tests for LibraryElement and StringInstanceElement

---
+++ src/test/c++/unit/library_element_tests.cpp
@@ -0,0 +1,105 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "global_test_objects.h"
+
+#include "service_wrapper_config.h"
+
+#include <sup/di-composer-core/constants.h>
+#include <sup/di-composer-core/element_constructor_map.h>
+#include <sup/di-composer-core/exceptions.h>
+#include <sup/di-composer-core/library_element.h>
+
+#include <sup/di/object_manager.h>
+
+#include <sup/xml/exceptions.h>
+#include <sup/xml/tree_data.h>
+
+#include <gtest/gtest.h>
+
+using namespace sup::di;
+
+class LibraryElementTest : public ::testing::Test
+{
+protected:
+  LibraryElementTest();
+  virtual ~LibraryElementTest();
+
+  sup::xml::TreeData m_library_tree;
+};
+
+sup::xml::TreeData CreateLibraryTree(const std::string& str_dep_name);
+
+std::string GetWrapperLibraryName();
+
+TEST_F(LibraryElementTest, Construction)
+{
+  EXPECT_NO_THROW(LibraryElement library_elem{m_library_tree});
+  auto constr_it = ElementConstructorMap().find(constants::LOAD_LIBRARY_TAG);
+  EXPECT_NO_THROW(constr_it->second(m_library_tree));
+}
+
+TEST_F(LibraryElementTest, Validation)
+{
+  {
+    sup::xml::TreeData library_tree{constants::LOAD_LIBRARY_TAG};
+    library_tree.SetContent("does_not_matter.so");
+    EXPECT_NO_THROW(ValidateLibraryTree(library_tree));
+    library_tree.AddAttribute("some_attribute", "not_allowed");
+    EXPECT_THROW(ValidateLibraryTree(library_tree), sup::xml::ValidationException);
+  }
+  {
+    sup::xml::TreeData library_tree{constants::LOAD_LIBRARY_TAG};
+    library_tree.SetContent("does_not_matter.so");
+    EXPECT_NO_THROW(ValidateLibraryTree(library_tree));
+    sup::xml::TreeData name_tree{constants::FUNCTION_NAME_TAG};
+    name_tree.SetContent("some_name");
+    library_tree.AddChild(name_tree);
+    EXPECT_THROW(ValidateLibraryTree(library_tree), sup::xml::ValidationException);
+  }
+}
+
+TEST_F(LibraryElementTest, Execution)
+{
+  LibraryElement library_elem{m_library_tree};
+  EXPECT_NO_THROW(library_elem.Execute());
+
+  LibraryElement library_elem_fail{CreateLibraryTree("this_name_does_not_exist")};
+  EXPECT_THROW(library_elem_fail.Execute(), RuntimeException);
+}
+
+LibraryElementTest::LibraryElementTest()
+  : m_library_tree{CreateLibraryTree(GetWrapperLibraryName())}
+{}
+
+LibraryElementTest::~LibraryElementTest() = default;
+
+sup::xml::TreeData CreateLibraryTree(const std::string& library_name)
+{
+  sup::xml::TreeData library_tree{constants::LOAD_LIBRARY_TAG};
+  library_tree.SetContent(library_name);
+  return library_tree;
+}
+
+std::string GetWrapperLibraryName()
+{
+  return service_wrapper_config::CMakeLibraryInstallDir() + "/libservice-wrapper.so";
+}
---
+++ src/test/c++/unit/string_instance_element_tests.cpp
@@ -0,0 +1,117 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "global_test_objects.h"
+
+#include <sup/di-composer-core/constants.h>
+#include <sup/di-composer-core/element_constructor_map.h>
+#include <sup/di-composer-core/exceptions.h>
+#include <sup/di-composer-core/string_instance_element.h>
+
+#include <sup/di/object_manager.h>
+
+#include <sup/xml/exceptions.h>
+#include <sup/xml/tree_data.h>
+
+#include <gtest/gtest.h>
+
+using namespace sup::di;
+
+const std::string STRING_INSTANCE_NAME = "Test_StringInstance_Name";
+const std::string STRING_INSTANCE_VALUE = "Test_StringInstance_Value";
+
+class StringInstanceElementTest : public ::testing::Test
+{
+protected:
+  StringInstanceElementTest();
+  virtual ~StringInstanceElementTest();
+
+  sup::xml::TreeData m_string_instance_tree;
+};
+
+sup::xml::TreeData CreateStringInstanceTree(const std::string& name, const std::string& value);
+
+TEST_F(StringInstanceElementTest, Construction)
+{
+  EXPECT_NO_THROW(StringInstanceElement inst_elem{m_string_instance_tree});
+  auto constr_it = ElementConstructorMap().find(constants::STRING_INSTANCE_TAG);
+  EXPECT_NO_THROW(constr_it->second(m_string_instance_tree));
+}
+
+TEST_F(StringInstanceElementTest, Validation)
+{
+  {
+    sup::xml::TreeData string_instance_tree{constants::STRING_INSTANCE_TAG};
+    EXPECT_THROW(ValidateStringInstanceTree(string_instance_tree), sup::xml::ValidationException);
+  }
+  {
+    sup::xml::TreeData string_instance_tree{constants::STRING_INSTANCE_TAG};
+    sup::xml::TreeData name_tree{constants::INSTANCE_NAME_TAG};
+    name_tree.SetContent("func_name");
+    string_instance_tree.AddChild(name_tree);
+    sup::xml::TreeData type_tree{constants::VALUE_TAG};
+    type_tree.SetContent("str_value");
+    string_instance_tree.AddChild(type_tree);
+    EXPECT_NO_THROW(ValidateStringInstanceTree(string_instance_tree));
+    string_instance_tree.SetContent("not_allowed");
+    EXPECT_THROW(ValidateStringInstanceTree(string_instance_tree), sup::xml::ValidationException);
+  }
+  {
+    sup::xml::TreeData string_instance_tree{constants::STRING_INSTANCE_TAG};
+    sup::xml::TreeData name_tree{constants::INSTANCE_NAME_TAG};
+    name_tree.SetContent("func_name");
+    string_instance_tree.AddChild(name_tree);
+    sup::xml::TreeData type_tree{constants::VALUE_TAG};
+    type_tree.SetContent("str_value");
+    string_instance_tree.AddChild(type_tree);
+    EXPECT_NO_THROW(ValidateStringInstanceTree(string_instance_tree));
+    string_instance_tree.AddAttribute("name", "not_allowed");
+    EXPECT_THROW(ValidateStringInstanceTree(string_instance_tree), sup::xml::ValidationException);
+  }
+}
+
+TEST_F(StringInstanceElementTest, Execution)
+{
+  StringInstanceElement inst_elem{m_string_instance_tree};
+  EXPECT_NO_THROW(inst_elem.Execute());
+
+  StringInstanceElement inst_elem_fail{
+    CreateStringInstanceTree(STRING_INSTANCE_NAME, "this_name_was_already_registered")};
+  EXPECT_THROW(inst_elem_fail.Execute(), RuntimeException);
+}
+
+StringInstanceElementTest::StringInstanceElementTest()
+  : m_string_instance_tree{CreateStringInstanceTree(STRING_INSTANCE_NAME, STRING_INSTANCE_VALUE)}
+{}
+
+StringInstanceElementTest::~StringInstanceElementTest() = default;
+
+sup::xml::TreeData CreateStringInstanceTree(const std::string& name, const std::string& value)
+{
+  sup::xml::TreeData string_instance_tree{constants::STRING_INSTANCE_TAG};
+  sup::xml::TreeData name_tree{constants::INSTANCE_NAME_TAG};
+  name_tree.SetContent(name);
+  string_instance_tree.AddChild(name_tree);
+  sup::xml::TreeData val_tree{constants::VALUE_TAG};
+  val_tree.SetContent(value);
+  string_instance_tree.AddChild(val_tree);
+  return string_instance_tree;
+}
