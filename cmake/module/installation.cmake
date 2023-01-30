031d6a99b40939a30bbc1d2c329c68f519af0cda
Rafael Graça Silva <rafael.g.silva@criticalsoftware.com>
2023-01-30 15:10:39 +0000

    move cmake sources into cmake root directory

--- cmake/module/installation.cmake
+++ cmake/module/installation.cmake
@@ -29,7 +29,7 @@ endif()
 include(CMakePackageConfigHelpers)
 
 # to use in the build tree
-configure_package_config_file(${CMAKE_CURRENT_LIST_DIR}/sup-di-config.cmake.in
+configure_package_config_file(${CMAKE_SOURCE_DIR}/cmake/config/sup-di-config.cmake.in
     ${CMAKE_CURRENT_BINARY_DIR}/sup-di-config.cmake
     INSTALL_DESTINATION ${INSTALL_CONFIGDIR}
 )
