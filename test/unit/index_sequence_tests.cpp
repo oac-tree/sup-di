764699af2128185067efe1da0c1068eb16c66c39
Walter Van Herck <walter.vanherck@iter.org>
2023-12-12 14:40:23 +0000

    Add unit test for IndexSequence

---
+++ test/unit/index_sequence_tests.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Walter Van Herck (IO)
+ *
+ * Copyright (c) : 2010-2023 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include <sup/di/index_sequence.h>
+
+#include <gtest/gtest.h>
+
+#include <type_traits>
+
+using namespace sup::di;
+
+class IndexSequenceElementTest : public ::testing::Test
+{
+protected:
+  IndexSequenceElementTest() {};
+  virtual ~IndexSequenceElementTest() {};
+};
+
+TEST_F(IndexSequenceElementTest, MakeIndexSequence)
+{
+  EXPECT_TRUE((std::is_same<internal::MakeIndexSequence<0>, internal::IndexSequence<>>::value));
+  EXPECT_TRUE((std::is_same<internal::MakeIndexSequence<1>, internal::IndexSequence<0>>::value));
+  EXPECT_TRUE((std::is_same<internal::MakeIndexSequence<5>, internal::IndexSequence<0, 1, 2, 3, 4>>::value));
+}
