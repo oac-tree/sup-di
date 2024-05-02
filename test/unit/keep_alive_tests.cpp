815ebc88658d612973d2b1c5c7b2dfd26488d301
RTorres <rjtorres@criticalsoftware.com>
2024-05-02 10:56:31 +0100

    Add di-servicese KeepAlive tests

---
+++ test/unit/keep_alive_tests.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+ * $HeadURL: $
+ * $Id: $
+ *
+ * Project       : Supervision and Automation - Dependency injection
+ *
+ * Description   : The definition and implementation for dependency injection templates in SUP.
+ *
+ * Author        : Ricardo Torres (Ext)
+ *
+ * Copyright (c) : 2010-2024 ITER Organization,
+ *                 CS 90 046
+ *                 13067 St. Paul-lez-Durance Cedex
+ *                 France
+ *
+ * This file is part of ITER CODAC software.
+ * For the terms and conditions of redistribution or use of this software
+ * refer to the file ITER-LICENSE.TXT located in the top level directory
+ * of the distribution package.
+ ******************************************************************************/
+
+#include "global_test_objects.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <sup/di-services/registered_names.h>
+#include <sup/di-services/keep_alive.h>
+#include <thread>
+#include <csignal>
+
+using namespace sup::di::services;
+
+TEST(KeepAliveTest, InterruptHandlerTest)
+{
+  testing::internal::CaptureStdout();
+  int signal = SIGINT;
+
+  interruptHandler(signal);
+
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_THAT(output, testing::HasSubstr("Terminating the execution with signal: " + std::to_string(signal)));
+}
+
+TEST(KeepAliveTest, ReturnsFalseWhenSIGINTSignalIsReceived)
+{
+  std::thread t(KeepAlive);
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  EXPECT_TRUE(t.joinable());
+  std::raise(SIGINT);
+  t.join();
+
+  EXPECT_FALSE(t.joinable());
+}
+
